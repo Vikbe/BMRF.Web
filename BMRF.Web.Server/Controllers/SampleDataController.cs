63dfeb0d0413f04c61942ef3f4046ad9dd07e13a
Viktor Berglind <berglind96@gmail.com>
2018-08-11 16:15:55 +0200

    test pages for rules and server list

--- BMRF.Web.Server/Controllers/SampleDataController.cs
+++ BMRF.Web.Server/Controllers/SampleDataController.cs
@@ -1,29 +1,68 @@
-﻿using BMRF.Web.Shared;
+﻿using BMRF.WEB.Shared;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BMRF.Web.Server.Controllers
+namespace BMRF.WEB.Server.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+      
+
+        private static GameServer[] GameServers = new GameServer[] {
+            new GameServer {
+                Game = "Rust",
+                Name = "RustySpoon 1", 
+                PlayerCap = 70
+            },
+             new GameServer {
+                Game = "Rust",
+                Name = "RustySpoon 2",
+                PlayerCap = 70
+            },
+              new GameServer {
+                Game = "Rust",
+                Name = "RustySpoon 3",
+                PlayerCap = 70
+            }
         };
 
+        private static string[] Rules = new string[] {
+            "Don't be a dick!",
+            "Join the army",
+            "Don't smoke the devils lettuce",
+            "No premarital sex"
+        };
+
+
+        // Example to populate a server list, randomly changes playercount
         [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
+        public IEnumerable<GameServer> GetServers()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
+            Random rnd = new Random();
+           
+            return Enumerable.Range(0, GameServers.Length).Select(index => {
+                var server = GameServers[index];
+                server.PlayerCount = rnd.Next(server.PlayerCount <= 4 ? server.PlayerCount : server.PlayerCount - 5, server.PlayerCount == server.PlayerCap - 5 ? server.PlayerCap : server.PlayerCount + 5);
+                return server;
+            }
+             
+            );
         }
+
+
+        // mOCK RULES
+        [HttpGet("[action]")]
+        public IEnumerable<string> GetRules()
+        {
+            return Enumerable.Range(0, Rules.Length).Select(index =>
+                Rules[index]);
+        }
+
+       
+
     }
 }
---
+++ BMRF.Web.Shared/GameServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMRF.WEB.Shared
+{
+   public class GameServer
+    {
+        public string Game { get; set; }
+        public string Name { get; set; }
+        public int PlayerCount { get; set; }
+        public int PlayerCap { get; set; }
+
+    }
+}
